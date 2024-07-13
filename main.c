74a70c3cb5aa017006b652914de5d77eb01222ae
David Buckman <dmbuck32@gmail.com>
2024-07-13 20:17:24 +1200

    add documentation.

--- main.c
+++ main.c
@@ -21,21 +21,21 @@
 #define PRESSED_EVENT 1
 #define REPEATED_EVENT 2
 
+#define SECONDS_TO_MS 1000
+#define MS_TO_MICROSEC 1000
+#define SECONDS_TO_NANOSEC 1000000
+
 // https://stackoverflow.com/questions/1157209/is-there-an-alternative-sleep-function-in-c-to-milliseconds
 /* msleep(): Sleep for the requested number of milliseconds. */
-int msleep(long msec)
+int msleep(const unsigned long msec)
 {
     struct timespec ts;
     int res;
 
-    if (msec < 0)
-    {
-        errno = EINVAL;
-        return -1;
-    }
-
-    ts.tv_sec = msec / 1000;
-    ts.tv_nsec = (msec % 1000) * 1000000;
+    // Get the non-fractional seconds.
+    ts.tv_sec = msec / SECONDS_TO_MS;
+    // Get the fractional value at nanosecond precision.
+    ts.tv_nsec = (msec % SECONDS_TO_MS) * SECONDS_TO_NANOSEC;
 
     do {
         res = nanosleep(&ts, &ts);
@@ -49,11 +49,11 @@ long long timeInMilliseconds(void) {
     struct timeval tv;
 
     gettimeofday(&tv, NULL);
-    return (((long long)tv.tv_sec)*1000)+(tv.tv_usec/1000);
+    return (((long long)tv.tv_sec)*SECONDS_TO_MS)+(tv.tv_usec/MS_TO_MICROSEC);
 }
 
 // logic comes from https://github.com/MichaelDipperstein/keypress/blob/master/keypress.c
-void console_echo(bool echo_off)
+void console_echo(const bool echo_off)
 {
     unsigned char echo_bit;
     if(echo_off)
@@ -80,18 +80,24 @@ typedef struct KeyState {
     int long_press_event;
 } KeyState;
 
-void handle_button_press(int value, KeyState* key, TvRemoteSm* tv_remote)
+// Handle the state transitions between key press & long-press.
+void handle_button_press(const int value, KeyState* key, TvRemoteSm* tv_remote)
 {
     switch (value)
     {
         case RELEASED_EVENT:
         {
+            // Clear the flags.
             key->pressed = false;
             key->long_press = false;
+            key->press_start_time = 0;
             break;
         }
         case PRESSED_EVENT:
         {
+            // Check if the key has already been pressed.
+            // If not, then register the press.
+            // This will be cleared on the key release event.
             if (!key->pressed)
             {
                 key->pressed = true;
@@ -102,8 +108,12 @@ void handle_button_press(int value, KeyState* key, TvRemoteSm* tv_remote)
         }
         case REPEATED_EVENT:
         {
-            if (!key->long_press)
+            // Check if the key has already been pressed and a long-press event hasn't been recorded.
+            // If the long-press event hasn't been recorded, then register the long-press.
+            // This is necessary because this event will continue to be triggered while the key is depressed.
+            if (key->pressed && !key->long_press)
             {
+                // Check if it has been long enough to be considered a "long-press";
                 if ((timeInMilliseconds() - key->press_start_time) > LONG_PRESS_TIMEOUT)
                 {
                     key->long_press = true;
@@ -124,12 +134,12 @@ int main(int argc, char ** argv)
     const bool ECHO_OFF = true;
     console_echo(ECHO_OFF);
 
-    printf("Initializing TV remote state machine...\n");
+    // Configure the State Machine for the TV remote.
     TvRemoteSm TvRemote;
     TvRemoteSm_ctor(&TvRemote);
     TvRemoteSm_start(&TvRemote);
 
-    printf("Initalizing keys...\n");
+    // Store the state of the buttons.
     KeyState b1 = {
         .pressed = false,
         .press_start_time = 0,
@@ -145,15 +155,12 @@ int main(int argc, char ** argv)
         .long_press_event = TvRemoteSm_EventId_B2_LONG_PRESS
     };
 
-    printf("Initializing key reader...\n");
-
-    // Get keyboard events.
+    // Open the keyboard input device.
+    // https://stackoverflow.com/questions/20943322/accessing-keys-from-linux-input-device/20946151#20946151
     const char *dev = "/dev/input/by-path/platform-i8042-serio-0-event-kbd";
-    struct input_event ev;
+    struct input_event event;
     ssize_t n;
-    int fd;
-
-    fd = open(dev, O_RDONLY);
+    const int fd = open(dev, O_RDONLY);
     if (fd == -1) {
         fprintf(stderr, "Cannot open %s: %s.\n", dev, strerror(errno));
         return EXIT_FAILURE;
@@ -162,40 +169,54 @@ int main(int argc, char ** argv)
     printf("Starting loop.\n");
     while(true)
     {
-        n = read(fd, &ev, sizeof ev);
+        // Read an event from the keyboard.
+        n = read(fd, &event, sizeof event);
         if (n == (ssize_t)-1) {
-            if (errno == EINTR)
+            if (errno == EINTR) {
+                // Continue processing in the case of an interrupted system call.
                 continue;
-            else
+            } else {
+                // Error.
                 break;
-            } else if (n != sizeof ev) {
+            }
+        } else if (n != sizeof event) {
+            // Failed to read enough data to constitute an event.
             errno = EIO;
             break;
         }
 
-        if (ev.type == EV_KEY && ev.value >= 0 && ev.value <= 2)
+        // Check if this is a key event with an event that we care about:
+        // - RELEASED_EVENT: 0
+        // - PRESSED_EVENT: 1
+        // - REPEATED_EVENT: 2
+        if (event.type == EV_KEY && event.value >= 0 && event.value <= 2)
         {
-            switch (ev.code)
+            switch (event.code)
             {
             case B1_CODE:
             {
-                handle_button_press(ev.value, &b1, &TvRemote);
+                handle_button_press(event.value, &b1, &TvRemote);
                 break;
             }
             case B2_CODE:
             {
-                handle_button_press(ev.value, &b2, &TvRemote);
+                handle_button_press(event.value, &b2, &TvRemote);
                 break;
             }
             default:
-
+                // Ignore other keys.
                 break;
             }
         }
     }
+    // Flush any remaining output.
     fflush(stdout);
     fprintf(stderr, "%s.\n", strerror(errno));
+
+    // Reset the console.
     const bool ECHO_ON = false;
     console_echo(ECHO_ON);
+
+    // Exit the application.
     return EXIT_SUCCESS;
 }
